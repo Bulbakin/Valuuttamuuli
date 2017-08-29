82a68e80088fd6bd7e134f774828abfcce8d9e56
Sirpoma Osku <s1601396@student.businesscollege.fi>
2017-08-29 13:33:58 +0300

    -

---
+++ ValuuttaMuunnin/src/GUI/Arvot.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+/**
+ *
+ * @author s1601396
+ */
+public class Arvot {
+    double AUD = 1.5008;
+    double BGN = 1.9558;
+    double BRL = 3.7568;
+    double CAD = 1.4866;
+    double CHF = 1.1389;
+    double CNY = 7.8979;
+    double CZK = 26.102;
+    double DKK = 7.4393;
+    double GBP = 0.9233;
+    double HKD = 9.3292;
+    double HRK = 7.4145;
+    double HUF = 304.01;
+    double IDR = 15903.18;
+    double ILS = 4.2719;
+    double INR = 76.2065;
+    double JPY = 130.34;
+    double KRW = 1335.56;
+    double MXN = 21.0584;
+    double MYR = 5.0889;
+    double NOK = 9.262;
+    double NZD = 1.6458;
+    double PHP = 60.841;
+    double PLN = 4.2581;
+    double RON = 4.5977;
+    double RUB = 69.659;
+    double SEK = 9.5155;
+    double SGD = 1.6156;
+    double THB = 39.591;
+    double TRY = 4.1063;
+    double TWD = 35.8835;
+    double USD = 1.1925;
+    double ZAR = 15.5562;
+    
+    public int arvo (String valuutta){
+        return 0;
+    }
+
+}
